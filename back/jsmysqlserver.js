d03c394e01a80dbf50e1a5d2e28b27579640f15b
watawata <mohamed.leghdaich@gmail.com>
2023-10-29 07:07:10 +0100

    debuging

--- back/jsmysqlserver.js
+++ back/jsmysqlserver.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+console.log(require('./mod').a);
+
+
+
+
 var url = require('url');
 const http=require('http');
 
@@ -14,19 +18,31 @@ const con = mysql.createConnection({
 //API Routes (API endpoints)
 //Get All
 const server = http.createServer((req, res)=>{
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
 
+  if(req.method=='GET'){
+    const queryparams = url.parse(req.url, true).query;
+    let q ="SELECT * FROM users WHERE name LIKE '%"+ queryparams._name +"%'";
+    if (queryparams._age) {q += " AND age = '"+ queryparams._age +"'";}
+    q += " ORDER BY id DESC LIMIT "+ queryparams._limit;
 
-  const queryparams = url.parse(req.url, true).query;
-  let q ="SELECT * FROM users WHERE name LIKE '%"+ queryparams._name +"%'";
-  if (queryparams._age) {q += " AND age = '"+ queryparams._age +"'";}
-  q += " ORDER BY id DESC LIMIT "+ queryparams._limit;
-  con.query(q, (err, rows)=>{
-    res.json(rows)
-  }) 
+    con.query(q, (err, rows)=>{
+      res.end(JSON.stringify(rows))
+    }) 
+  }
+  else if (req.method=='POST'){
+    con.query("INSERT INTO users (name, age, photo) VALUES ('"+ req.body.name +"', '"+ req.body.age +"', '"+ req.body.photo +"')", (err, data)=>{
+      res.json(data.insertId)
+    })
+  }
+  else if (req.method=='PUT'){
+    
+  }
+  else if (req.method=='DELETE'){
+    
+  }
 }); 
 
 con.connect((err) => {
---
+++ back/mod.js
@@ -0,0 +1,5 @@
+const a = 11;
+function f() {
+    return 'ffff';
+}
+module.exports.a = a;
