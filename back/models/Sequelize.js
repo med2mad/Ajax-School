83b0736a76eccf0045be9c561021b59d0ed9ab4c
watawata <mohamed.leghdaich@gmail.com>
2024-01-19 21:02:22 +0100

    sequlize associations debug

--- back/models/Sequelize.js
+++ back/models/Sequelize.js
@@ -1,28 +1,6 @@
 const {sequelizeCon, SequelizeClass} = require('../configurations/sequelizeconn');
 const Op = SequelizeClass.Op;
 
-// const User = sequelizeCon.define('user', {
-//     _id:{
-//         type: SequelizeClass.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true,
-//         allowNull: false,
-//     },
-//     name:{
-//         type: SequelizeClass.STRING,
-//         validate:{ len:{args:[1,25], msg:"name from 1 to 25 chars !"} },
-//     },
-//     age:{
-//         type: SequelizeClass.INTEGER,
-//         validate:{ min:{args:18, msg:"under aged !"}, max:{args:99, msg:"over aged !"} },
-//     },
-//     photo:{
-//         type: SequelizeClass.STRING,
-//         defaultValue: '',
-//     }
-// },
-// );
-
 const Game = sequelizeCon.define('game', {
         name:{
             type: SequelizeClass.STRING,
@@ -37,25 +15,23 @@ const Category = sequelizeCon.define('category', {
         },
     },
 );
-const Joint = sequelizeCon.define('joint'
-);
 
-Game.belongsToMany(Category, {through:Joint, targetKey:'name', foreignKey:'category'});
-Category.belongsToMany(Game, {through:Joint, targetKey:'name', foreignKey:'game'});
+Game.hasOne(Category);
+// Category.belongsToMany(Game, {through:Join, sourceKey:'name', foreignKey:'game'}); //targetKey:'name',
 
 sequelizeCon.sync({force:true})
 .then(()=>{
-    // return Game.bulkCreate([{name:'zelda'},{name:'castlevania'},{name:'tekken'},]);
+    return Game.bulkCreate([{name:'zelda'},{name:'castlevania'},{name:'tekken'},]);
 })  
 .then(()=>{
-    // return Category.bulkCreate([{name:'3D'},{name:'RPG'},{name:'Windows'},]);
+    return Category.bulkCreate([{name:'3D'},{name:'RPG'},{name:'Windows'},]);
 })  
 .then(()=>{
-    // return Category.findAll();
+    return Category.findAll();
 })
 .then((data)=>{
-    // categories = data;
-    // return Game.findOne({where:{name:"zelda"}});
+    categories = data;
+    return Game.findOne({where:{name:"zelda"}});
 })
 .then((data)=>{
     // data.addCategories(categories);
