1c129077ea64cb486a1cd50025a8502228f8ab0d
watawata <mohamed.leghdaich@gmail.com>
2024-01-19 22:53:05 +0100

    restore last commit

--- back/models/Sequelize.js
+++ back/models/Sequelize.js
@@ -1,6 +1,28 @@
 const {sequelizeCon, SequelizeClass} = require('../configurations/sequelizeconn');
 const Op = SequelizeClass.Op;
 
+// const User = sequelizeCon.define('user', {
+//     _id:{
+//         type: SequelizeClass.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false,
+//     },
+//     name:{
+//         type: SequelizeClass.STRING,
+//         validate:{ len:{args:[1,25], msg:"name from 1 to 25 chars !"} },
+//     },
+//     age:{
+//         type: SequelizeClass.INTEGER,
+//         validate:{ min:{args:18, msg:"under aged !"}, max:{args:99, msg:"over aged !"} },
+//     },
+//     photo:{
+//         type: SequelizeClass.STRING,
+//         defaultValue: '',
+//     }
+// },
+// );
+
 const Game = sequelizeCon.define('game', {
         name:{
             type: SequelizeClass.STRING,
