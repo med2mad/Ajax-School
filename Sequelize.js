283ab55dc0133792a939438822d528110be1f73e
watawata <mohamed.leghdaich@gmail.com>
2024-01-23 15:17:57 +0100

    sequelize-cli

--- Sequelize.js
+++ Sequelize.js
@@ -1,43 +1,43 @@
 const {sequelizeCon, SequelizeClass} = require('./back/configurations/sequelizeconn');
-const Op = SequelizeClass.Op;
+const Op = SequelizeClass.Op;let result;
 
-const Game = sequelizeCon.define('game', {
+const Model1 = sequelizeCon.define('model1', {
         name:{
             type: SequelizeClass.STRING,
-            unique:true,
+            primaryKey:true
+            
         },
     },
 );
-const Category = sequelizeCon.define('category', {
+const Model2 = sequelizeCon.define('model2', {
         name:{
             type: SequelizeClass.STRING,
-            unique:true,
+            primaryKey:true
         },
     },
 );
 
-Game.hasOne(Category);
+Model1.belongsToMany(Model2, {through:'Join'});
+Model2.belongsToMany(Model1, {through:'Join'});
 // Category.belongsToMany(Game, {through:Join, sourceKey:'name', foreignKey:'game'}); //targetKey:'name',
-
 sequelizeCon.sync({force:true})
+.then(()=>{return Model1.bulkCreate([{name:'zelda'},{name:'castlevania'},{name:'tekken'},]);})  
+.then(()=>{return Model2.bulkCreate([{name:'3D'},{name:'RPG'},{name:'Windows'},]);})  
+
+
 .then(()=>{
-    return Game.bulkCreate([{name:'zelda'},{name:'castlevania'},{name:'tekken'},]);
-})  
-.then(()=>{
-    return Category.bulkCreate([{name:'3D'},{name:'RPG'},{name:'Windows'},]);
-})  
-.then(()=>{
-    return Category.findAll();
+    return Model1.findByPk('tekken');
 })
 .then((data)=>{
-    categories = data;
-    return Game.findOne({where:{name:"zelda"}});
+    console.log( data.hasModel2s('3D'));
+    // data.createModel2({name:'new'});
+    // return data.countModel2s();
 })
 .then((data)=>{
-    // data.addCategories(categories);
+    // console.log(data);
 })
+
 
 ;
-
 module.exports.sequelizeCon = sequelizeCon;
 module.exports.Op = Op;
