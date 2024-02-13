c9a8ec2327b8f1f8a5ec1225497d033cb7c6c582
watawata <mohamed.leghdaich@gmail.com>
2024-02-13 19:06:43 +0100

    finish valdation video tutorials

--- back/mysql.js
+++ back/mysql.js
@@ -7,16 +7,16 @@ const {getAlls, adds, edits, removes} = require('./controllers/orm/mysql');
 //Get
 app.get('/', getAll);
 //Insert
-app.post('/', body('name').custom((value)=>{console.log(value);}) , add);
+app.post('/', add);
 //Update
 app.put('/:id', edit);
 //Delete
 app.delete('/:id', remove);
 
 // const a = [query('name').trim().notEmpty(), param('name').trim().notEmpty(), body('name').escape().trim().notEmpty()];
-app.post('/sub/:id', body('name').notEmpty(), (req, res, next)=>{
-    console.log('loged');
-    // res.send(validationResult(req));
+app.post('/sub/:id', query('name1','msg'), (req, res, next)=>{
+
+    res.send(validationResult(req));
 
 });
 
