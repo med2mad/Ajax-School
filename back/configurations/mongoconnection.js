3afec124f55d862314abb1877ed71612cc91b60b
watawata <mohamed.leghdaich@gmail.com>
2024-03-13 23:34:13 +0000

    last commit

--- back/configurations/mongoconnection.js
+++ back/configurations/mongoconnection.js
@@ -1,8 +1,8 @@
 const {app} = require('./expressapp');
 
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.connect('mongodb+srv://med:allahommairhamna@cluster0.7zp3rzt.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect('mongodb+srv://med:allahommairhamna@cluster0.7zp3rzt.mongodb.net/', {useNewUrlParser: true, useUnifiedTopology: true})
 
 .then(() => { //if connection works to start listening
   app.listen(5020, () => {console.log("Mongoose: " + 5020);});
