b24e8f7c1d6ee82f7b872cd38848ca772fc86814
watawata <mohamed.leghdaich@gmail.com>
2024-04-20 23:58:30 +0100

    fetch

--- src/components/scripts/AJAX/Fetch.js
+++ src/components/scripts/AJAX/Fetch.js
@@ -13,45 +13,45 @@ function fget(uri, store, limit, currentpage, back, ajax){
 }
 
 function fpost(url, body, store, limit, back, ajax){
-        fetch(url, {
-          method: "POST", 
-          body: body
-          })
-        .then((response)=> {return response.json()})
-        .then((response)=>{
-                const rowToInsert = {"id":response.newId, "_id":response.newId, "photo":response.photo, "name":body.get("name"), "age":body.get("age")};//FormData object use get 
-                store.rows.unshift(rowToInsert);
-                if(store.rows.length>limit){store.rows.pop();} //remove last row in <table> (respect _limit after add)
-              })
-      }
+  fetch(url, {
+    method: "POST", 
+    body: body
+  })
+  .then((response)=> {return response.json()})
+  .then((response)=>{
+    const rowToInsert = {"id":response.newId, "_id":response.newId, "photo":response.photo, "name":body.get("name"), "age":body.get("age")};//FormData object use get 
+    store.rows.unshift(rowToInsert);
+    if(store.rows.length>limit){store.rows.pop();} //remove last row in <table> (respect _limit after add)
+  })
+}
 
 function fput(method, uri, body, selectedTr, store, back, ajax){
-        fetch(uri, {
-            method: method,
-            body: body
-        })
-        .then((response)=> {return response.json()})
-        .then((response)=> {
-          store.rows[selectedTr].name=body.get('name'); store.rows[selectedTr].age=body.get('age');store.rows[selectedTr].photo=response.photo;
-        })
-      }
+  fetch(uri, {
+    method: method,
+    body: body
+  })
+  .then((response)=> {return response.json()})
+  .then((response)=> {
+    store.rows[selectedTr].name=body.get('name'); store.rows[selectedTr].age=body.get('age');store.rows[selectedTr].photo=response.photo;
+  })
+}
 
 function fdelete(method, url, store, back, ajax){
-        fetch(url, {method: method})
-        .then((response)=>{return response.json()})
-        .then((response)=>{
-          //GET replacement row
-          if(response.length>0)
-          { store.rows.push({"id":response[0].id, "_id":response[0]._id, "name":response[0].name, "age":response[0].age, "photo":response[0].photo}) }
-        })
-      }
+  fetch(url, {method: method})
+  .then((response)=>{return response.json()})
+  .then((response)=>{
+    //GET replacement row
+    if(response.length>0)
+    { store.rows.push({"id":response[0].id, "_id":response[0]._id, "name":response[0].name, "age":response[0].age, "photo":response[0].photo}) }
+  })
+}
 
-    function fixHeader(){
-    // const defaultHeaders = new Headers(); //create an object to work with in all calls
-    // defaultHeaders.append('Content-Type', 'application/json; charset=utf-8');
-    ////setting "Content-Type" to "multipart/form-data" throws "Multipart: Boundary not found" error
+function fixHeader(){
+  // const defaultHeaders = new Headers(); //create an object to work with in all calls
+  // defaultHeaders.append('Content-Type', 'application/json; charset=utf-8');
+  ////setting "Content-Type" to "multipart/form-data" throws "Multipart: Boundary not found" error
 
-    //body: JSON.stringify(body), headers: {"Content-Type":"application/json"} //if data send as JSON not as FormData (no photos)
-  }
+  //body: JSON.stringify(body), headers: {"Content-Type":"application/json"} //if data send as JSON not as FormData (no photos)
+}
 
-  export default {fget, fpost, fput, fdelete}
+export default {fget, fpost, fput, fdelete}
