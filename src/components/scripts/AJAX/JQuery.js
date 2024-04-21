107a34e260beacbf0229b2824579d6bf329136ba
watawata <mohamed.leghdaich@gmail.com>
2024-04-21 02:37:20 +0100

    jquery

--- src/components/scripts/AJAX/JQuery.js
+++ src/components/scripts/AJAX/JQuery.js
@@ -2,7 +2,6 @@ import $ from "jquery";
 import { paginate } from '../pagination';
 
 function fget(uri, store, limit, currentpage, back, ajax){
-  
   $.ajax({url:uri , method:'GET', dataType:'json'})
     .done(function(response, textStatus, jqXHR){
       store.snippet += `------ GET ---- ${Date.now() - store.time} ms --------\n$.ajax({url:${uri} , method:'GET', dataType:'json'})
@@ -14,56 +13,54 @@ function fget(uri, store, limit, currentpage, back, ajax){
 }
 
 function fpost(uri, body, store, limit, back, ajax){
-        $.ajax(
-            {
-              type:'POST',
-              contentType: false,
-              processData: false,
-              url:uri,
-              data: body,
-            }
-          )
-        .done(function(response){
-              const rowToInsert = {"id":response.newId, "_id":response.newId, "photo":response.photo, "name":body.get("name"), "age":body.get("age")};//FormData object use get
-              store.rows.unshift(rowToInsert);
-              if(store.rows.length>limit){store.rows.pop();} //remove last row in <table> (respect _limit after add)
-        });
-      }
+  $.ajax(
+    {
+      type:'POST',
+      contentType: false,
+      processData: false,
+      url:uri,
+      data: body,
+    }
+  )
+  .done(function(response){
+    const rowToInsert = {"id":response.newId, "_id":response.newId, "photo":response.photo, "name":body.get("name"), "age":body.get("age")};//FormData object use get
+    store.rows.unshift(rowToInsert);
+    if(store.rows.length>limit){store.rows.pop();} //remove last row in <table> (respect _limit after add)
+  });
+}
 
 function fput(method, uri, body, selectedTr, store, back, ajax){
-        console.log('put jquery');
-        $.ajax(
-            {
-              "type":method, 
-              "url":uri, 
-              "data":body,
-            }
-          )
-        .done( function(response){
-            store.rows[selectedTr].name=body.get('name'); store.rows[selectedTr].age=body.get('age'); store.rows[selectedTr].photo=response.photo;
-        });
-      }
-      
+  $.ajax(
+    {
+      "type":method, 
+      "url":uri, 
+      "data":body,
+    }
+  )
+  .done( function(response){
+    store.rows[selectedTr].name=body.get('name'); store.rows[selectedTr].age=body.get('age'); store.rows[selectedTr].photo=response.photo;
+  });
+}
+  
 function fdelete(method, uri, store, back, ajax){
-        $.ajax(
-            {
-            "type":method, 
-            "url":uri
+  $.ajax(
+    {
+    "type":method, 
+    "url":uri
 
-            //using JSON data (no FormData = no photos)
-            // url:uri+'?callback=?'
-            }
-          )
-        .done( function(response){
-          //GET replacement row
-          if(response.length>0)
-          { store.rows.push({"id":response[0].id, "_id":response[0]._id, "name":response[0].name, "age":response[0].age, "photo":response[0].photo}) }
-        });
-      }
+    //using JSON data (no FormData = no photos)
+    // url:uri+'?callback=?'
+    }
+  )
+  .done( function(response){
+    //GET replacement row
+    if(response.length>0)
+    { store.rows.push({"id":response[0].id, "_id":response[0]._id, "name":response[0].name, "age":response[0].age, "photo":response[0].photo}) }
+  });
+}
 
+function fixHeader(){
+  // $.ajaxSetup({contentType:false, processData:false,});
+}
 
- function fixHeader(){
-    // $.ajaxSetup({contentType:false, processData:false,});
-  }
-
-  export default {fget, fpost, fput, fdelete}
+export default {fget, fpost, fput, fdelete}
